2e103a210be019daaa28fba8a76f54e3a3601f18
MakerALLEN <imakerlab@gmail.com>
2016-12-17 17:37:22 +0200

    first commit
    yo

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + 'index.html');
+});
+
+http.createServer(app).listen(process.env.PORT || 3000, function(){
+  console.log('Express server listening on port ' + process.env.PORT || 3000);
+});
